0aaa377553aa251d00149c4012061413847e8220
kauany <kauany04.talita@gmai.com>
2025-01-20 19:05:00 -0300

    Novos exercicios

---
+++ Ex008/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Ex008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double velocidade, valor_multa;
+            string multa;
+            
+
+            Console.WriteLine("Digite a velocidade do carro: ");
+            velocidade = Convert.ToDouble(Console.ReadLine());
+
+            valor_multa = (velocidade - 80) * 7;
+
+            if (velocidade > 80) {
+
+                Console.WriteLine("Você foi multado! Deve ser pagp o valor de {0} referente aos km exedidos: ", valor_multa);
+            }
+            else
+            {
+                Console.WriteLine("Dirija com prudencia!! ");
+            }
+
+        }
+    }
+}
---
+++ Ex009/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ex009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n;
+
+            Console.WriteLine("Digite um número:");
+            n = Convert.ToDouble(Console.ReadLine());
+
+            if (n % 2 == 0)
+            {
+
+                Console.WriteLine("Número par.");
+            }
+            else
+            {
+                Console.WriteLine("Número impar.");
+            }
+        }
+    }
+}
