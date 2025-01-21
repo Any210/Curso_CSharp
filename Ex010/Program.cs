a4890b66e15f946c1ca47340e747568e9f49f63c
kauany <kauany04.talita@gmai.com>
2025-01-20 21:49:52 -0300

    Calculando o Salário

---
+++ Ex010/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ex010
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario;
+            
+            Console.WriteLine("Digite o valor do seu salário: ");
+            salario = Convert.ToDouble(Console.ReadLine());
+
+            if (salario > 2500)
+            {
+                Console.WriteLine("O aumento do seu salário é de 5%, o seu salário atual é: {0}", ((salario * 0.05) + salario));
+            }
+            else if (salario >  1250)
+            {
+                Console.WriteLine("O aumento do seu salário é de 10%, o seu salário atual é: {0}", ((salario * 0.10) + salario));
+            }
+            else
+            {
+                Console.WriteLine("O aumento do seu salário é de 15%, o seu salário atual é: {0}", ((salario * 0.15) + salario));
+            }
+        }
+    }
+}
