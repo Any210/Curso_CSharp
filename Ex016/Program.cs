3d54f991d963efddc0b491a387a1c5a2704f1f7e
kauany <kauany04.talita@gmai.com>
2025-01-22 22:00:47 -0300

    Exercicios dia 22/01

---
+++ Ex016/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ex016
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string genero;
+
+            Console.Write("Digite o Gênero: ");
+            genero = Console.ReadLine().ToUpper();
+
+            while (genero != "F" && genero != "M")
+            {
+                Console.Write("Digite o Gênero: ");
+                genero = Console.ReadLine().ToUpper();
+            }
+
+            Console.WriteLine("Bem-vindos e bem-vindas ao curso de C#!");
+        }
+    }
+}
