547be6c8e7d7a0413ab39b59e6bcc8f0f4703183
kauany <kauany04.talita@gmai.com>
2025-01-29 21:06:37 -0300

    Exercicios dia 29/01

---
+++ impressao/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace impressao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            escreva("Kauany Victoria Santos");
+            Console.WriteLine();
+            escreva("Curso C#");
+            Console.WriteLine();
+            escreva("Olá");
+            Console.WriteLine();
+        }
+
+        static void escreva(string texto)
+        {
+            // escreve o texto centralizado em uma linha com o caracter informado
+
+            char caracter = '='; // Caracter que seera impresso
+            int tamanho = texto.Length + 4; //tamanho da linha
+            string linha = new string(caracter, tamanho); // cria uma linha com o caracter informado
+            string texto_centralizado  = texto.PadLeft(2 + texto.Length).PadRight(tamanho); //centraliza o texto
+            Console.WriteLine(linha);
+            Console.WriteLine(texto_centralizado);
+            Console.WriteLine(linha);
+        }
+    }
+}
